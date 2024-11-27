6d5b9db7c0d384ecea787aa263b347ff7a16b935
Sandarva Podder <sandarvapodder09032004@gmail.com>
2024-11-28 03:02:01 +0530

    Frontend api endpoint

--- application/frontend/src/components/component/ImageUpload.jsx
+++ application/frontend/src/components/component/ImageUpload.jsx
@@ -7,7 +7,7 @@ function ImageUpload({ onUploadComplete, clearDiagnosis }) {
   const [selectedFile, setSelectedFile] = useState(null);
   const [loading, setLoading] = useState(false);
   const fileInputRef = useRef(null); // Ref to access the file input
-  const ML_SERVER_URL = "https://flask-ml-cei3.onrender.com";
+  const ML_SERVER_URL = process.env.NEXT_PUBLIC_ML_SERVER_URL;
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
     clearDiagnosis();
