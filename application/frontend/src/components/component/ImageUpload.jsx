81186381fba9a508d6fe6ad3a55f04f638c5f8c0
Sandarva Podder <sandarvapodder09032004@gmail.com>
2025-08-10 03:04:08 +0530

    Configuring envs

--- application/frontend/src/components/component/ImageUpload.jsx
+++ application/frontend/src/components/component/ImageUpload.jsx
@@ -9,7 +9,7 @@ function ImageUpload({ onUploadComplete, clearDiagnosis }) {
   const fileInputRef = useRef(null); // Ref to access the file input
   // const ML_SERVER_URL = "https://sandy9304-flaskapi.hf.space";
   const ML_SERVER_URL =
-    process.env.PUBLICMLSERVERURL || "http://localhost:5000";
+    "https://sandy9304-flaskapi.hf.space" || "http://localhost:5000";
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
     clearDiagnosis();
